dcd518d1927082ac4b941914a4961ca9735177ef
sufian <sufian4se@gmail.com>
2022-08-27 22:52:18 +0600

    Internationl travel, display countries name, capital

---
+++ js/countries.js
@@ -0,0 +1,24 @@
+const loadCountries = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  // for(const country of countries) {
+  //     console.log(country)
+  // }
+  const countriesContainer = document.getElementById("countries-container");
+  countries.forEach((country) => {
+    console.log(country)
+    const countryDiv = document.createElement("div");
+    countryDiv.classList.add("country");
+    countryDiv.innerHTML = `
+            <h3>Name: ${country.name.common}</h3>
+            <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+        `;
+    countriesContainer.appendChild(countryDiv);
+  });
+};
+
+loadCountries();
